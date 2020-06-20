d64c6d31bc4b873c1e0a27a71819a910be51ebb5
SepehrMoalemi <sepehr.moalemi@mail.mcgill.ca>
2020-06-20 18:30:32 -0400

    main.sh: Added comments

--- main.sh
+++ main.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+clear
 
+## Load Eigen Library
 module load eigen/3.3.5
 
+## Build the C++ Program
 g++ -c TestGit.cpp Func.cpp Func2.cpp
 g++ TestGit.o Func.o Func2.o -o main.exe
+
 ./main.exe
 
+## Removing the objects
 rm Func2.o Func.o TestGit.o main.exe 
